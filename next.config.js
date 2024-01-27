0911923567b403e9d41964cad44456dfdc0d04dc
hfellerhoff <henryfellerhoff@gmail.com>
2024-01-27 18:51:35 -0500

    add pwa, favicon

--- next.config.js
+++ next.config.js
@@ -4,7 +4,27 @@
  */
 await import("./src/env.js");
 
+import withPWA from "@ducanh2912/next-pwa";
+
+const wrapConfig = withPWA({
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  dest: "public",
+  fallbacks: {
+    //image: "/static/images/fallback.png",
+    // document: "/offline", // if you want to fallback to a custom page rather than /_offline
+    // font: '/static/font/fallback.woff2',
+    // audio: ...,
+    // video: ...,
+  },
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+  // ... other options you like
+});
+
 /** @type {import("next").NextConfig} */
 const config = {};
 
-export default config;
+export default wrapConfig(config);
