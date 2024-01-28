f476134ea32cf7024f625d10a1cbee806e030787
hfellerhoff <henryfellerhoff@gmail.com>
2024-01-27 23:24:13 -0500

    update pwa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { TRPCReactProvider } from "~/trpc/react";
 import { cn } from "~/lib/utils";
 import { ThemeProvider } from "~/components/ThemeProvider";
-import { type Viewport } from "next";
+import { type Metadata, type Viewport } from "next";
 import { Toaster } from "sonner";
 
 const inter = Inter({
@@ -14,10 +14,72 @@ const inter = Inter({
   variable: "--font-sans",
 });
 
-export const metadata = {
-  title: "Serial",
-  description: "Your personal content newsletter",
-  icons: [{ rel: "icon", url: "/favicon.ico" }],
+const title = "Serial";
+const description = "Your personal content newsletter";
+
+export const metadata: Metadata = {
+  title: title,
+  applicationName: title,
+  appleWebApp: {
+    capable: true,
+    title: title,
+    startupImage: "/apple-touch-icon.png",
+    statusBarStyle: "default",
+  },
+  description: description,
+  formatDetection: {
+    telephone: false,
+  },
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  keywords: ["video", "rss", "newsletter", "content", "youtube", "podcast"],
+  authors: [
+    {
+      name: "Henry Fellerhoff",
+      url: "https://www.henryfellerhoff.com",
+    },
+  ],
+  icons: [
+    { rel: "icon", url: "/favicon.ico" },
+    {
+      rel: "icon",
+      sizes: "16x16",
+      type: "image/png",
+      url: "/favicon-16x16.png",
+    },
+    {
+      rel: "icon",
+      sizes: "32x32",
+      type: "image/png",
+      url: "/favicon-32x32.png",
+    },
+    { rel: "apple-touch-icon", sizes: "180x180", url: "/apple-touch-icon.png" },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "512x512",
+      url: "/android-chrome-512x512.png",
+    },
+  ],
+  twitter: {
+    card: "summary",
+    creator: "@henryfellerhoff",
+    title: title,
+    description: description,
+  },
+  openGraph: {
+    title: title,
+    description: description,
+    type: "website",
+    images: [
+      {
+        url: "/icon-256.png",
+        width: 256,
+        height: 256,
+        alt: title,
+      },
+    ],
+  },
 };
 
 export const viewport: Viewport = {
