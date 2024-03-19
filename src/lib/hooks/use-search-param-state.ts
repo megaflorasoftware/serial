0e3f093f09f36bb68b167763988a947f6958a0a4
hfellerhoff <henryfellerhoff@gmail.com>
2024-03-19 02:52:12 -0400

    fix build issues with some ungodly eslint overrides

--- src/lib/hooks/use-search-param-state.ts
+++ src/lib/hooks/use-search-param-state.ts
@@ -1,5 +1,11 @@
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { type Dispatch, type SetStateAction, useState, useEffect } from "react";
+import {
+  type Dispatch,
+  type SetStateAction,
+  useState,
+  useEffect,
+  useCallback,
+} from "react";
 import { type z, type ZodType } from "zod";
 
 export function useSearchParamState<T extends ZodType>(
@@ -22,9 +28,10 @@ export function useSearchParamState<T extends ZodType>(
     router.push(url);
   };
 
-  const getValue = (): z.infer<T> => {
+  const getValue = useCallback(() => {
     const value = searchParams.get(key);
     if (value === null) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
       return defaultValue;
     }
 
@@ -33,26 +40,31 @@ export function useSearchParamState<T extends ZodType>(
       return value;
     }
 
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
     return defaultValue;
-  };
+  }, [defaultValue, key, schema, searchParams]);
 
   const [value, setStateValue] = useState<z.infer<T>>(() => getValue());
 
   useEffect(() => {
     const value = getValue();
     setStateValue(value);
-  }, [searchParams]);
+  }, [getValue, searchParams]);
 
   const setValue: Dispatch<SetStateAction<z.infer<T>>> = (
     newValueOrCallback,
   ) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     const newValue =
       typeof newValueOrCallback === "function"
         ? // @ts-expect-error I don't know, can't be bothered
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-call
           newValueOrCallback(value)
         : newValueOrCallback;
 
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
     setStateValue(newValue);
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
     updateUrl(newValue);
   };
 
