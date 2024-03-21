6dbbff12342a28f32c94a3eacc4157a89e5d3bd4
hfellerhoff <henryfellerhoff@gmail.com>
2024-03-20 23:27:34 -0400

    lol remove promise

--- src/lib/data/FeedProviderServer.tsx
+++ src/lib/data/FeedProviderServer.tsx
@@ -1,9 +1,8 @@
 import { api } from "~/trpc/server";
-import { PropsWithChildren } from "react";
+import { type PropsWithChildren } from "react";
 import { FeedProvider } from "./FeedProvider";
 
 export async function FeedProviderServer({ children }: PropsWithChildren) {
-  await new Promise((resolve) => setTimeout(resolve, 5000));
   const data = await api.feed.getAllFeedData.query();
 
   return <FeedProvider data={data}>{children}</FeedProvider>;
