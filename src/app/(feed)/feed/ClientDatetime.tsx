6539bd31015a63de1b842da39073d6e4ae4f9e7d
hfellerhoff <henryfellerhoff@gmail.com>
2024-03-20 23:28:56 -0400

    updating time

--- src/app/(feed)/feed/ClientDatetime.tsx
+++ src/app/(feed)/feed/ClientDatetime.tsx
@@ -1,7 +1,22 @@
 "use client";
 
 import dayjs from "dayjs";
+import { useEffect, useState } from "react";
+
+function getDate() {
+  return dayjs().format("dddd, MMMM DD • hh:mma");
+}
 
 export function ClientDatetime() {
-  return dayjs().format("dddd, MMMM DD • hh:mma");
+  const [dateString, setDateString] = useState(getDate());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDateString(getDate());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return dateString;
 }
