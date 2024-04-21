dab971ba0a12802314e44e9d31203f913b2e1ff9
hfellerhoff <henryfellerhoff@gmail.com>
2024-04-20 22:52:20 -0400

    hide actions when appropriate

--- src/app/(feed)/feed/watch/[videoID]/VideoActions.tsx
+++ src/app/(feed)/feed/watch/[videoID]/VideoActions.tsx
@@ -2,6 +2,7 @@ import { ClockIcon, EyeIcon, EyeOffIcon, CheckIcon } from "lucide-react";
 import { useKeyboard } from "~/components/KeyboardProvider";
 import { Button } from "~/components/ui/button";
 import { useFeed } from "~/lib/data/FeedProvider";
+import { useMediaQuery } from "~/lib/hooks/use-media-query";
 
 export function VideoActions({ videoID }: { videoID: string }) {
   const { view } = useKeyboard();
@@ -12,9 +13,13 @@ export function VideoActions({ videoID }: { videoID: string }) {
   const isWatched = video?.isWatched;
   const isWatchLater = video?.isWatchLater;
 
+  const shouldHideFullscreenActions = useMediaQuery("(min-aspect-ratio: 4/3)");
+
   if (!video) return null;
 
   if (view === "fullscreen") {
+    if (shouldHideFullscreenActions) return null;
+
     return (
       <div className="absolute inset-x-0 bottom-0 z-0 flex w-full items-center justify-center gap-2 p-6">
         <Button
