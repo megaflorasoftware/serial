03d011737153d3cfd48de62337039ae838fab7c4
hfellerhoff <henryfellerhoff@gmail.com>
2025-03-17 01:19:25 -0400

    smaller carousel on mobile

--- src/app/(markdown)/welcome/DemoCarousel.tsx
+++ src/app/(markdown)/welcome/DemoCarousel.tsx
@@ -6,48 +6,48 @@ export function DemoCarousel() {
       <div className="absolute inset-x-0 top-12 md:top-16">
         <Marquee>
           <img
-            className="mx-2 h-72 rounded shadow md:mx-4 md:h-80"
+            className="mx-2 h-40 rounded shadow sm:h-56 md:mx-4 md:h-80"
             alt="Screenshot of an example video light mode feed in Serial"
             src="/welcome/feed-light-1-desktop.jpeg"
           />
           <img
-            className="mx-2 h-72 rounded shadow md:mx-4 md:h-80"
+            className="mx-2 h-40 rounded shadow sm:h-56 md:mx-4 md:h-80"
             alt="Screenshot of an example video dark mode feed in Serial"
             src="/welcome/feed-dark-1-mobile.jpeg"
           />
           <img
-            className="mx-2 h-72 rounded shadow md:mx-4 md:h-80"
+            className="mx-2 h-40 rounded shadow sm:h-56 md:mx-4 md:h-80"
             alt="Screenshot of an example video light mode feed in Serial"
             src="/welcome/feed-light-1-mobile.jpeg"
           />
           <img
-            className="mx-2 h-72 rounded shadow md:mx-4 md:h-80"
+            className="mx-2 h-40 rounded shadow sm:h-56 md:mx-4 md:h-80"
             alt="Screenshot of an example video dark mode feed in Serial"
             src="/welcome/feed-dark-1-desktop.jpeg"
           />
           <img
-            className="mx-2 h-72 rounded shadow md:mx-4 md:h-80"
+            className="mx-2 h-40 rounded shadow sm:h-56 md:mx-4 md:h-80"
             alt="Screenshot of an example video light mode feed in Serial"
             src="/welcome/feed-light-2-desktop.jpeg"
           />
           <img
-            className="mx-2 h-72 rounded shadow md:mx-4 md:h-80"
+            className="mx-2 h-40 rounded shadow sm:h-56 md:mx-4 md:h-80"
             alt="Screenshot of an example video dark mode feed in Serial"
             src="/welcome/feed-dark-2-mobile.jpeg"
           />
           <img
-            className="mx-2 h-72 rounded shadow md:mx-4 md:h-80"
+            className="mx-2 h-40 rounded shadow sm:h-56 md:mx-4 md:h-80"
             alt="Screenshot of an example video dark mode feed in Serial"
             src="/welcome/feed-dark-2-desktop.jpeg"
           />
           <img
-            className="mx-2 h-72 rounded shadow md:mx-4 md:h-80"
+            className="mx-2 h-40 rounded shadow sm:h-56 md:mx-4 md:h-80"
             alt="Screenshot of an example video light mode feed in Serial"
             src="/welcome/feed-light-2-mobile.jpeg"
           />
         </Marquee>
       </div>
-      <div className="h-80 md:h-96" />
+      <div className="h-48 sm:h-68 md:h-96" />
     </>
   );
 }
