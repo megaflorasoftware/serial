ddfa17b7a02842f1ea6fd3c241d3f3b25966102a
hfellerhoff <henryfellerhoff@gmail.com>
2025-04-17 23:01:28 -0400

    add github help/idea links

--- src/components/LeftSidebarBottomNav.tsx
+++ src/components/LeftSidebarBottomNav.tsx
@@ -9,7 +9,15 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "~/components/ui/sidebar";
-import { NotebookIcon, PaletteIcon } from "lucide-react";
+import {
+  CircleHelpIcon,
+  HelpingHandIcon,
+  LifeBuoyIcon,
+  LightbulbIcon,
+  MailIcon,
+  NotebookIcon,
+  PaletteIcon,
+} from "lucide-react";
 import Link from "next/link";
 import { ColorThemeDropdownSidebar } from "./color-theme/ColorThemePopoverButton";
 
@@ -31,6 +39,30 @@ export function LeftSidebarBottomNav() {
               <Link href="/releases">
                 <NotebookIcon />
                 <span>Release Log</span>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+          <SidebarMenuItem>
+            <SidebarMenuButton asChild>
+              <Link
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://github.com/hfellerhoff/serial/issues/new?template=bug-report.md"
+              >
+                <LifeBuoyIcon />
+                <span>Report Issue</span>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+          <SidebarMenuItem>
+            <SidebarMenuButton asChild>
+              <Link
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://github.com/hfellerhoff/serial/issues/new?template=feature_request.md"
+              >
+                <LightbulbIcon />
+                <span>Share Idea</span>
               </Link>
             </SidebarMenuButton>
           </SidebarMenuItem>
