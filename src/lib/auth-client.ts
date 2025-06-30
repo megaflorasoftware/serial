913eeebb9620d4674639e64ab30bc684346e9e5a
Henry <48483883+hfellerhoff@users.noreply.github.com>
2025-06-30 10:15:01 -0400

    Update auth-client.ts

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,9 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 import { env } from "~/env";
 
-export const authClient = createAuthClient({
-  baseURL: env.NEXT_PUBLIC_ROOT_URL,
-});
+export const authClient = createAuthClient({});
 
 export const { signIn, signOut, signUp, useSession, resetPassword } =
   authClient;
