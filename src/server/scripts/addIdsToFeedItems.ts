90d8a1a37e62ac6d0e7c46092dff07e4adb975ac
hfellerhoff <henryfellerhoff@gmail.com>
2025-08-27 23:50:08 -0400

    try chunking

--- src/server/scripts/addIdsToFeedItems.ts
+++ src/server/scripts/addIdsToFeedItems.ts
@@ -1,41 +1,70 @@
 import "dotenv/config";
 
 import * as schema from "~/server/db/schema";
-import { and, eq, isNull } from "drizzle-orm";
+import { and, desc, eq, isNotNull, isNull, sql } from "drizzle-orm";
 import { db } from "../db";
 
 import { createId } from "@paralleldrive/cuid2";
 
+const PAGE_SIZE = 1000;
+
 async function migrate() {
   let feedItemsMigrated = 0;
 
-  await db.transaction(async (tx) => {
-    const feedItems = await tx
-      .select()
-      .from(schema.feedItems)
-      .where(isNull(schema.feedItems.id))
-      .all();
+  const countQuery = await db
+    .select({
+      count: sql`COUNT(${schema.feedItems.id})`,
+    })
+    .from(schema.feedItems)
+    .where(isNotNull(schema.feedItems.id))
+    .all();
 
-    return await Promise.all(
-      feedItems.map(async (feedItem) => {
-        try {
-          const res = await tx
-            .update(schema.feedItems)
-            .set({
-              id: createId(),
-            })
-            .where(
-              and(
-                eq(schema.feedItems.contentId, feedItem.contentId),
-                eq(schema.feedItems.feedId, feedItem.feedId ?? 0),
-              ),
-            );
-          feedItemsMigrated += 1;
-          return res;
-        } catch {}
-        return true;
-      }),
-    );
+  const itemCount = countQuery[0]!.count as number;
+  console.log(`${itemCount} items to migrate...`);
+
+  await db.transaction(async (tx) => {
+    let offset = 0;
+    let isFinished = false;
+
+    while (!isFinished) {
+      const feedItems = await tx
+        .select()
+        .from(schema.feedItems)
+        .where(isNotNull(schema.feedItems.id))
+        .limit(PAGE_SIZE)
+        .offset(offset)
+        .orderBy(desc(schema.feedItems.contentId))
+        .all();
+
+      console.log(`Migrating items ${offset} to ${offset + PAGE_SIZE}...`);
+      offset += PAGE_SIZE;
+
+      if (!feedItems.length) {
+        isFinished = true;
+        return;
+      }
+
+      await Promise.all(
+        feedItems.map(async (feedItem) => {
+          try {
+            const res = await tx
+              .update(schema.feedItems)
+              .set({
+                id: createId(),
+              })
+              .where(
+                and(
+                  eq(schema.feedItems.contentId, feedItem.contentId),
+                  eq(schema.feedItems.feedId, feedItem.feedId ?? 0),
+                ),
+              );
+            feedItemsMigrated += 1;
+            return res;
+          } catch {}
+          return true;
+        }),
+      );
+    }
   });
 
   console.log(`Migrated ${feedItemsMigrated} rows!`);
