e2575abe8c8d5eb84deae830adb374420c3139d7
hfellerhoff <henryfellerhoff@gmail.com>
2025-11-25 15:57:08 -0500

    fix header alignment

--- src/app/(feed)/feed/Header.tsx
+++ src/app/(feed)/feed/Header.tsx
@@ -17,9 +17,13 @@ export function Header() {
 
   return (
     <header className="top-0 z-20 flex w-full flex-wrap items-center justify-between gap-2 bg-transparent px-6 py-6">
-      <TopLeftButton />
+      <span className="inline-flex w-auto items-center justify-start md:w-16">
+        <TopLeftButton />
+      </span>
       <FeedLoader />
-      <TopRightHeaderContent />
+      <span className="inline-flex w-auto items-center justify-end md:w-16">
+        <TopRightHeaderContent />
+      </span>
     </header>
   );
 }
