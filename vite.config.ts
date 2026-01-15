1f8e1533430ea198ad1f219ca58320dc446549a2
Henry <48483883+hfellerhoff@users.noreply.github.com>
2026-01-15 01:05:17 -0500

    add nitro

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   server: {
@@ -22,6 +23,7 @@ export default defineConfig({
       //   enabled: true,
       // },
     }),
+    nitro(),
     viteReact(),
   ],
 });
