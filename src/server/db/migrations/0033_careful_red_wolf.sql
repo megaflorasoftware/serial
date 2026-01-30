4dd445a7416009a20f7731f2b3a0b1f884ea857c
Henry <48483883+hfellerhoff@users.noreply.github.com>
2026-01-29 19:35:33 -0500

    add needed indexes

---
+++ src/server/db/migrations/0033_careful_red_wolf.sql
@@ -0,0 +1,3 @@
+CREATE INDEX `account_user_id_idx` ON `serial_account` (`user_id`);--> statement-breakpoint
+CREATE INDEX `session_user_id_idx` ON `serial_session` (`user_id`);--> statement-breakpoint
+CREATE INDEX `view_categories_category_id_idx` ON `serial_view_categories` (`category_id`);
