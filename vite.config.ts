12f75a908ad79a008830614527129e5ebbb8a903
Henry <48483883+hfellerhoff@users.noreply.github.com>
2026-04-06 11:09:16 -0400

    remove marquee dep, small updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 import { nitro } from "nitro/vite";
 import contentCollections from "@content-collections/vite";
@@ -10,12 +9,12 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  resolve: {
+    tsconfigPaths: true,
+  },
   plugins: [
     contentCollections(),
     tailwindcss(),
-    tsconfigPaths({
-      projects: ["./tsconfig.json"],
-    }),
     tanstackStart({
       srcDirectory: "src",
       router: {
